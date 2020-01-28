52dfa28694f28f92e9c5e4cf8cf1621c12049abe
Aziz R <41672061+mmdkcxx@users.noreply.github.com>
2020-01-28 13:51:14 +0700

    Needle & Spinner for Fortunewheel unity

---
+++ needle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class needle : MonoBehaviour {
+	public spinner _spinner;
+	public Text scoretext;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay2D(Collider2D col){
+		if (!_spinner.isStoped)
+			return;
+		scoretext.text = col.gameObject.name;
+
+	}
+
+}
